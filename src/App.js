bf9262637de0e0e8c67c3f4c674c78f3e259ed13
Kim Nguyen <kimnivore@gmail.com>
2022-05-26 15:43:40 -0700

    keane edit of pink

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import './App.css';
 
 const APIkey = '526154d7e1a163ec19dbe2350ad2b307';
 const APPkey = 'GxhFZmDqR_wEHBjn-fnCKYBMJMIWCe4SDTapJbD_yWc';
@@ -57,9 +58,8 @@ export default function App() {
   
   return (
     <AppContainer>
-    <WrapperContainer>
-
-        <SearchContainer>
+        <h1>What is the temperature today?</h1>
+        <div className='search'>
           <input
             type="text"
             value={locations}
@@ -70,15 +70,13 @@ export default function App() {
           <button className="location_searcher" onClick={handleSubmit}>
             Search Location
           </button>
-        </SearchContainer>
+        </div>
 
-        <WeatherContainer>
-          <p className="temp">Current Temperature: {weather?.main?.temp}</p>
-        </WeatherContainer>
+        <div className='weather'>
+          <p className="temp">Current Temperature: {weather?.main?.temp} &deg;F</p>
+        </div>
 
         <img className="app__image" src={photos} alt="" />
-
-      </WrapperContainer>
     </AppContainer>
   );
 
@@ -86,20 +84,33 @@ export default function App() {
 
 const AppContainer = styled.div`
 text-align: center;
-width:
+margin: 40px auto;
+border: 1px solid #f038ff;
+width: 35%;
+background-color: white;
+
+h1 {
+  font-size: 2rem;
+  color: #f038ff;
+}
+p {
+  font-size: 1.5rem;
+  color: #f038ff;
+  font-weight: bold;
+}
 }
 img {
-  width: 30%;
+  width: 50%;
+  border: 1px solid #f038ff;
 }
-`
-const WrapperContainer = styled.div`
-`
-const SearchContainer = styled.div`
+.search{
 /* display: flex;
 flex-direction: column;
 justify-content: center;
 margin: auto;
 width: 30%; */
+}
 `
-const WeatherContainer = styled.div`
-`
+
+
+
