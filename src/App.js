660b2fbdc28930956a7cdd3607aed1696a5be9d6
Kim Nguyen <kimnivore@gmail.com>
2022-05-27 15:07:46 -0700

    added e.prevent

--- src/App.js
+++ src/App.js
@@ -15,34 +15,34 @@ export default function App() {
 
 
   useEffect(() => {
-    handleSubmit();
-    // axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${WEATHER_API_KEY}&units=imperial`)
-    // .then(res => {
-    //   console.log(res);
-    //   setWeather(res.data);
+    // handleSubmit();
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${WEATHER_API_KEY}&units=imperial`)
+    .then(res => {
+      console.log(res);
+      setWeather(res.data);
       
-    // })
-    // .catch(err => {
-    //   console.log(err)
-    //   alert('There has been an error. Please re-enter the location.')
-    // })
+    })
+    .catch(err => {
+      console.log(err)
+      alert('There has been an error. Please re-enter the location.')
+    })
 
-    // axios.get(`https://api.unsplash.com/search/photos?query=${location}&client_id=${UNSPLASH_API_KEY}`)
-    // .then(res => {
-    //   console.log(res); 
-    //   setPhotos(res.data.results[Math.floor(Math.random() * res.data.results.length)].urls.raw);
+    axios.get(`https://api.unsplash.com/search/photos?query=${location}&client_id=${UNSPLASH_API_KEY}`)
+    .then(res => {
+      console.log(res); 
+      setPhotos(res.data.results[Math.floor(Math.random() * res.data.results.length)].urls.raw);
      
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    // })
+    })
+    .catch(err => {
+      console.log(err);
+    })
 
   }, []);
 
 
 
-  const handleSubmit = () => {
-    // e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${WEATHER_API_KEY}&units=imperial`)
     .then(res => {
       setWeather(res.data)
@@ -72,6 +72,7 @@ export default function App() {
         {/* <p className="weather-icon">{icon}
           <img className='weather-icon' src={`http://openweathermap.org/img/wn/${weather?.weather[0]?.icon}@2x.png`} alt='weather icon'/>
         </p>  */}
+
         {/* <p className="weather-icon">{ (weather.weather[0].description)[0].toUpperCase() + (weather.weather?.[0].description).substring(1) } 
           <img className='weather-icon' src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather icon'/>
         </p>  */}
